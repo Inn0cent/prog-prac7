8ccffe9a058509ac97047bcd3f0e4f9a75e6edce
Inn0cent <Inn0cent@users.noreply.github.com>
2017-12-08 17:09:12 +0000

    First commit

---
+++ Christmas/Tree.java
@@ -0,0 +1,61 @@
+
+/**
+ * Write a description of class Tree here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public class Tree
+{
+    
+    private int height;
+    private int width;
+    
+    public Tree(int height)
+    {
+        this.height = height;
+        if (height%2 == 1){
+            width = height - 1;
+        } else{
+            width = height;
+        }
+    }
+    
+    public void draw(){
+        String[] layers = new String[width];
+        for(int i = 0; i < height/2; i++){
+            layers[(layers.length/2)+i] = "*";
+            layers[(layers.length/2)-i] = "*";
+            output(layers);
+            output(layers);
+        }
+        if (height%2 == 1){
+            for(int i = 0; i <= width; i++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        String[] stump = new String[width];
+        stump[stump.length/2] = "*";
+        output(stump);
+        output(stump);
+        for(int i = 0; i <= width/4; i++){
+            stump[(stump.length/2) + i] = "*";
+            stump[(stump.length/2) - i] = "*";
+        }
+        output(stump);
+        output(stump);
+    }
+    
+    public void output(String[] layer){
+        for(String i : layer){
+            if(i == null){
+                System.out.print(" ");
+            }else{
+                System.out.print(i);
+            }
+        }
+        System.out.println();
+    }
+}
